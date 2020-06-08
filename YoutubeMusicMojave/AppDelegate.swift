38ae850758d8c83b754afb8130a98f6a9b0f993d
MINGKYME <admin@mingky.me>
2020-06-08 17:46:29 +0900

    Initial Commit

---
+++ YoutubeMusicMojave/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YoutubeMusicMojave
+//
+//  Created by MINGKY KIM on 2020/06/08.
+//  Copyright © 2020 mingky.me. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YoutubeMusicMojave/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YoutubeMusicMojave
+//
+//  Created by MINGKY KIM on 2020/06/08.
+//  Copyright © 2020 mingky.me. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
